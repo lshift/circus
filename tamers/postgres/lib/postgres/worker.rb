104a5e45a5c8cd5a6662bcd49946580c6be9aea5
Paul Jones <pauljones23@gmail.com>
2010-10-01 21:20:40 +0100

    Postgres uses lowercase for names - downcase for compares

--- tamers/postgres/lib/postgres/worker.rb
+++ tamers/postgres/lib/postgres/worker.rb
@@ -10,14 +10,14 @@ module Postgres
 
     def create_database(name, user, password, logger)
       # Ensure that we have an owner user
-      if DB[:pg_user].where(:usename => user).count == 0
+      if DB[:pg_user].where(:usename => user.downcase).count == 0
         logger.info("Creating database user #{user}")
           # TODO: Can't use ? substitution - need to validate username
         DB["CREATE USER #{user} WITH PASSWORD ?", password].first
       end
 
       # Ensure that we have the database
-      if DB[:pg_database].where(:datname => name).count == 0
+      if DB[:pg_database].where(:datname => name.downcase).count == 0
         logger.info("Creating database #{name}")
 
         DB["CREATE DATABASE #{name} WITH OWNER=#{user}"].first
