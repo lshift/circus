67e3f60a7098dbfe1c647a1875265f25a8be43a1
Paul Jones <pauljones23@gmail.com>
2010-10-09 08:48:21 +0100

    Make downloaded jars readable by all

--- stacks/java_web/cookbooks/jetty/recipes/default.rb
+++ stacks/java_web/cookbooks/jetty/recipes/default.rb
@@ -25,24 +25,30 @@ end
 remote_file "/opt/jetty-7.1/lib/jndi/amqp-client-1.8.1.jar" do
   source "http://repo1.maven.org/maven2/com/rabbitmq/amqp-client/1.8.1/amqp-client-1.8.1.jar"
   action :create_if_missing
+  mode 0644
 end
 remote_file "/opt/jetty-7.1/lib/jndi/commons-io-1.2.jar" do
   source "http://repo1.maven.org/maven2/commons-io/commons-io/1.2/commons-io-1.2.jar"
   action :create_if_missing
+  mode 0644
 end
 remote_file "/opt/jetty-7.1/lib/jndi/commons-pool-1.3.jar" do
   source "http://repo2.maven.org/maven2/commons-pool/commons-pool/1.3/commons-pool-1.3.jar"
   action :create_if_missing
+  mode 0644
 end
 remote_file "/opt/jetty-7.1/lib/jndi/commons-dbcp-1.2.2.jar" do
   source "http://repo1.maven.org/maven2/commons-dbcp/commons-dbcp/1.2.2/commons-dbcp-1.2.2.jar"
   action :create_if_missing
+  mode 0644
 end
 remote_file "/opt/jetty-7.1/lib/jndi/derby-10.4.2.0.jar" do
   source "http://repo1.maven.org/maven2/org/apache/derby/derby/10.4.2.0/derby-10.4.2.0.jar"
   action :create_if_missing
+  mode 0644
 end
 remote_file "/opt/jetty-7.1/lib/jndi/postgresql-8.4-701.jdbc4.jar" do
   source "http://mirrors.ibiblio.org/pub/mirrors/maven2/postgresql/postgresql/8.4-701.jdbc4/postgresql-8.4-701.jdbc4.jar"
   action :create_if_missing
+  mode 0644
 end
